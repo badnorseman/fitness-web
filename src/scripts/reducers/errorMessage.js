afa1c3c0c11df800c180b08898f267e44c25fe97
Dan Jensen <dj@fitbird.com>
2015-09-11 16:30:09 +0200

    fix resetErrorMessage when linking from navbar

--- src/scripts/reducers/errorMessage.js
+++ src/scripts/reducers/errorMessage.js
@@ -2,11 +2,14 @@
 import {
   RESET_ERROR_MESSAGE
 } from "../actions/errorActions";
+import {
+  ROUTE_CHANGE
+} from "../actions/routeActions";
 
 export default function errorMessage(state = null, action) {
   const { type, errors } = action;
 
-  if (type === RESET_ERROR_MESSAGE) {
+  if (type === RESET_ERROR_MESSAGE || type === ROUTE_CHANGE) {
     return null;
   } else if (errors) {
     return errors;
