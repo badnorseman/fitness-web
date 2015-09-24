07827cbcf14e7e8bce33e31c4c851cba37bddd62
Dan Jensen <dj@fitbird.com>
2015-09-24 08:05:48 -0400

    update ProductListItem

--- src/scripts/components/products/ProductListItem.js
+++ src/scripts/components/products/ProductListItem.js
@@ -20,12 +20,12 @@ export default class ProductListItem extends Component {
     const { item } = this.props;
     const { description, name } = item;
     const titleStyle = {
-      backgroundImage:'url(' + this.props.item.image + ')',
-      backgroundRepeat:'no-repeat',
-      backgroundSize:'cover',
-      height:160,
-      WebkitTransition:'all',
-      msTransition:'all'
+      backgroundImage: 'url(' + this.props.item.image + ')',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      height: 160,
+      WebkitTransition: 'all',
+      msTransition: 'all'
     }
 
     return (
