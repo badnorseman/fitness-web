186867cddaec6b3c980beac94bf0a5413c046c1b
Dan Jensen <dj@fitbird.com>
2015-09-29 13:38:21 -0400

    rename App to Layout

--- src/scripts/containers/Layout.js
+++ src/scripts/containers/Layout.js
@@ -2,7 +2,7 @@
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { changeRoute } from "../actions/routeActions";
-import App from "../components/App";
+import Layout from "../components/Layout";
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ changeRoute }, dispatch);
@@ -14,4 +14,4 @@ function mapStateToProps(state) {
   return { param, route };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     "webpack-dev-server/client?http://localhost:8080/",
     "webpack/hot/dev-server",
-    "./src/scripts/index.js"
+    "./src/scripts/App.js"
   ],
   output: {
     path: path.join(__dirname, "dist"),
