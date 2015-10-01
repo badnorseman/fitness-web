c44951d630980b1a5dafa69ac26bcb5714e23b6f
Dan Jensen <dj@fitbird.com>
2015-09-30 20:10:25 -0400

    update Oauth

--- src/scripts/components/auth/Oauth.js
+++ src/scripts/components/auth/Oauth.js
@@ -1,25 +1,28 @@
 "use strict";
 import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
 import { oauth } from "../../actions/authActions";
 import Button from "../Button";
 
-export default class Oauth extends Component {
+class Oauth extends Component {
   static propTypes = {
     provider: PropTypes.string.isRequired
   }
 
   constructor(props) {
     super(props);
-    this._handleClick = this._handleClick.bind(this);
+    this._handleOauth = this._handleOauth.bind(this);
   }
 
-  _handleClick() {
-    oauth(this.props.provider);
+  _handleOauth() {
+    this.props.dispatch(oauth(this.props.provider));
   }
 
   render() {
     return (
-      <Button name={this.props.provider} type="button" onClick={this._handleClick} />
+      <Button name={this.props.provider} type="button" onClick={this._handleOauth} />
     )
   }
 }
+
+export default connect()(Oauth);
