5fdcda06366f707cf1d12a32b286ce12af3d7b13
M1st1ck <veselinivanov@gmail.com>
2015-10-25 19:17:01 +0100

    signup styled

--- src/scripts/components/auth/Signup.js
+++ src/scripts/components/auth/Signup.js
@@ -7,8 +7,6 @@ import { connect } from "react-redux";
 import { changeRoute } from "../../actions/routeActions";
 import { signup } from "../../actions/authActions";
 import Button from "../../components/Button";
-import Facebook from "./Facebook";
-import Google from "./Google";
 import InputField from "../../components/InputField";
 import "./signup.css";
 
@@ -16,11 +14,14 @@ class Signup extends Component {
   constructor(props) {
     super(props);
     this._handleClose = this._handleClose.bind(this);
+    this._goToLogin = this._goToLogin.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
   }
-
   _handleClose() {
     this.props.dispatch(changeRoute("MARKETPLACE"));
+  }
+  _goToLogin() {
+    this.props.dispatch(changeRoute("LOGIN"));
   }
 
   _handleSubmit(event) {
@@ -38,46 +39,45 @@ class Signup extends Component {
       }))
     };
   }
-
   render() {
     return (
       <div className="mdl-grid">
         <div className="mdl-cell mdl-cell--12-col">
-          <div className="signup mdl-card mdl-shadow--2dp">
-            <div className="mdl-card__supporting-text mdl-card--border">
-              <Button name="Close" type="button" onClick={this._handleClose} />
-              <div><Facebook /></div>
-              <div><Google /></div>
+          <div className="login full-screen mdl-card mdl-shadow--2dp">
+            <div className="mdl-cell--hide-phone">
+              <Button name="Sign Up" disabled="true" className="mdl-button mdl-js-button mdl-card__return disabled--color-black text--left margin-left--15px" type="button" onClick={this._handleClose} />
+            </div>
+            <div className="mdl-cell--hide-tablet mdl-cell--hide-desktop">
+              <Button name="&larr; Sign Up" className="mdl-button mdl-js-button mdl-card__return mdl-button--raised mdl-js-ripple-effect mdl-button--primary text--left" type="button" onClick={this._handleClose} />
+            </div>
+            <div className="mdl-card__supporting-text mdl-card--border padding-top--0">
               <form onSubmit={this._handleSubmit}>
                 <div>
-                  <InputField
-                    fieldId="email"
-                    fieldName="Email"
-                    fieldType="text"
-                    ref="email" />
+                  <InputField fieldId="firstName" fieldName="First Name" fieldType="text" ref="firstName" />
                 </div>
                 <div>
-                  <InputField
-                    fieldId="password"
-                    fieldName="Password"
-                    fieldType="password"
-                    ref="password" />
+                  <InputField fieldId="lastName" fieldName="Last Name" fieldType="text" ref="lastName" />
                 </div>
                 <div>
-                  <InputField
-                    fieldId="passwordConfirmation"
-                    fieldName="Confirm password"
-                    fieldType="password"
-                    ref="passwordConfirmation" />
+                  <InputField fieldId="email" fieldName="Email" fieldType="text" ref="email" />
                 </div>
                 <div>
-                  <Button name="Signup" type="submit" />
+                  <InputField fieldId="password" fieldName="Password" fieldType="password" ref="password" />
+                </div>
+                <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect text--left margin--8px" htmlFor="checkboxNews">
+                  <input type="checkbox" id="checkboxNews" className="mdl-checkbox__input"/>
+                  <span className="mdl-checkbox__label">Tell me about FitBird news</span>
+                </label>
+                <div>
+                  <Button name="SIGN UP" className="mdl-cell mdl-cell--12-col mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit" />
                 </div>
               </form>
+              Already a member? 
+              <Button name="Sign in!" className=" mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--accent" type="button" onClick={this._goToLogin} />
             </div>
           </div>
         </div>
-      </div>
+        </div>
     )
   }
 }
