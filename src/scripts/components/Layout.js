214fd484329a87abe4526c8a492b0e79fbef9698
M1st1ck <veselinivanov@gmail.com>
2015-10-25 19:13:02 +0100

    Drawer, authentication page, hide header on phone when login/sign up, hidden header modifier

--- src/scripts/components/Layout.js
+++ src/scripts/components/Layout.js
@@ -12,12 +12,14 @@ import NewProduct from "./products/NewProduct";
 import NewTransaction from "../containers/NewTransaction";
 import ShowProduct from "./products/ShowProduct";
 import Signup from "../components/auth/Signup";
+import Authenticate from "../components/auth/Authenticate";
 import "./Layout.css";
 
 export default class Layout extends Component {
   constructor(props) {
     super(props);
     this._goToMarketplace = this._goToMarketplace.bind(this);
+    this._goToAuthenticate = this._goToAuthenticate.bind(this);
   }
 
   componentDidUpdate() {
@@ -27,10 +29,15 @@ export default class Layout extends Component {
   _goToMarketplace() {
     this.props.changeRoute("MARKETPLACE");
   }
+ _goToAuthenticate() {
+  document.getElementById("drawer").className = "mdl-layout__drawer";
+    this.props.changeRoute("AUTHENTICATE");
+  }
 
   render() {
     const { param, route } = this.props;
-
+    var header = document.getElementById("header")
+    if (header) header.className = "mdl-layout__header layout__header";
     let content;
     switch (route) {
       case "ACCOUNT":
@@ -40,6 +47,7 @@ export default class Layout extends Component {
         content = <Dashboard />;
         break;
       case "LOGIN":
+        document.getElementById("header").className += " header--hidden-phone";
         content = <Login />;
         break;
       case "EDITPRODUCT":
@@ -55,7 +63,12 @@ export default class Layout extends Component {
         content = <ShowProduct product={param} />;
         break;
       case "SIGNUP":
+        document.getElementById("header").className += " header--hidden-phone";
         content = <Signup />;
+        break;
+      case "AUTHENTICATE":
+        document.getElementById("header").className += " header--hidden-phone";
+        content = <Authenticate />;
         break;
       default:
         content = <Marketplace />;
@@ -63,7 +76,7 @@ export default class Layout extends Component {
     return (
       <div>
         <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-          <header className="mdl-layout__header layout__header">
+          <header id="header" className="mdl-layout__header layout__header">
             <div className="mdl-layout__header-row">
               <span className="mdl-layout-title">
                 <a className="mdl-navigation__link" href="#!" onClick={this._goToMarketplace}>FitBird</a>
@@ -71,8 +84,18 @@ export default class Layout extends Component {
               <div className="mdl-layout-spacer"></div>
               <Navigation />
             </div>
+            <div className="mdl-layout-icon"></div>
           </header>
-          <main className="mdl-layout__content">
+          <div id="drawer" className="mdl-layout__drawer">
+            <span className="mdl-layout-title">FitBird</span>
+            <nav className="mdl-navigation">
+              <a className="mdl-navigation__link" href="#!" onClick={this._goToAuthenticate}>Acc</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+              <a className="mdl-navigation__link" href="">Link</a>
+            </nav>
+          </div>
+          <main className="mdl-layout__content background-color--white-phone">
             <ErrorMessage />
             <div className="page-content">
               {content}
