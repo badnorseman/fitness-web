2554034a5f7356b6c7ebca951f9b5060aef7b2d6
M1st1ck <veselinivanov@gmail.com>
2015-11-05 11:35:00 +0100

    Select Component added. Birthday added to sign up

---
+++ src/scripts/components/Select.js
@@ -0,0 +1,48 @@
+"use strict";
+import React, { Component, findDOMNode, PropTypes } from "react";
+import "./Select.css";
+
+export default class Select extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    key: PropTypes.string,
+    options: PropTypes.array,
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool,
+    styles: PropTypes.string
+  }
+
+  constructor(props) {
+    super(props);
+    this._handleChange = this._handleChange.bind(this);
+    let options = this.props.options ? this.props.options.map(x => <option key={x.value} value={x.value}>{x.name}</option>) : [];
+    this.state = { fieldValue: "", options: options };
+  }
+
+  _handleChange(event) {
+    this.setState({ fieldValue: event.target.value });
+    if (this.props.onChange) this.props.onChange(event);
+  }
+  render() {
+    const { id, key, name, styles, options, disabled } = this.props;
+
+    return (
+       <div className={styles}>
+          <select 
+            onChange={this._handleChange} 
+            id={id}
+            key={key}
+            name={name}
+            className="select"
+            disabled={disabled}
+            value={this.state.fieldValue}>
+            { !this.state.fieldValue && <option disabled value=""></option>}
+            {this.state.options}
+          </select>
+          { !this.state.fieldValue && <label className="select__label">{name}</label>}
+          { this.state.fieldValue && <label className="select__label select__label--fly">{name}</label>}
+        </div>
+    )
+  }
+}
--- src/scripts/components/auth/Signup.js
+++ src/scripts/components/auth/Signup.js
@@ -10,13 +10,66 @@ import Facebook from "./Facebook";
 import Google from "./Google";
 import IconButton from "../IconButton";
 import InputField from "../../components/InputField";
+import Select from "../Select";
 import "./signup.css";
 
 class Signup extends Component {
+
+  static defaultProps = {
+      months : [
+        {value:1, name:"January"},
+        {value:2, name:"February"},
+        {value:3, name:"March"},
+        {value:4, name:"April"},
+        {value:5, name:"May"},
+        {value:6, name:"June"},
+        {value:7, name:"July"},
+        {value:8, name:"August"},
+        {value:9, name:"September"},
+        {value:10, name:"October"},
+        {value:11, name:"November"},
+        {value:12, name:"December"}
+      ],
+      years : function() {
+        let years = [];
+        for(var i= new Date().getFullYear(); i > 1900; i--) {
+          years.push({value:i, name:i});
+        }
+        return years;
+      }
+  }
+
   constructor(props) {
     super(props);
+    this._monthChanged = this._monthChanged.bind(this);
+    this._yearChanged = this._yearChanged.bind(this);
     this._handleClose = this._handleClose.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
+
+    this.state = { month: false, year: false, days: [] };
+  }
+
+   _monthChanged(e) {
+    var month = e.target.value;
+    this.setState({month: month});
+    this._setDays(this.state.year, month);
+  }
+
+  _yearChanged(e) {
+    var year = e.target.value;
+    this.setState({year: year});
+    this._setDays(year, this.state.month);
+  }
+
+  _setDays(year,month) {
+    if (year && month) {
+      var days = new Date(year, month, 0).getDate()
+      var Days = [];
+      for(var i=1; i <= days; i++) {
+        Days.push({value:i, name:i});
+      }
+      this.setState({days: Days});
+    }
   }
 
   _handleClose() {
@@ -77,6 +130,30 @@ class Signup extends Component {
                     styles="signup__input-field"
                     ref="passwordConfirmation" />
                 </div>
+
+                <label>Birthday</label>
+                <div>
+                  <Select 
+                    styles="birthday__select" 
+                    onChange={this._yearChanged} 
+                    options={this.props.years()} 
+                    id="birthday-year" 
+                    name="Year" />
+                  <Select 
+                    styles="birthday__select birthday__select--side-margin" 
+                    onChange={this._monthChanged} 
+                    options={this.props.months} 
+                    id="birthday-month" 
+                    name="Month" />
+                  <Select 
+                    styles="birthday__select" 
+                    disabled={!this.state.days.length} 
+                    key={this.state.days.length} 
+                    options={this.state.days} 
+                    id="birthday-day" 
+                    name="Day" />
+                </div>
+
                 <button
                   className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent signup-button"
                   type="submit">
