45197b5930e508fa7a6bb2c43796870e5ed8536a
Dan Jensen <dj@fitbird.com>
2015-11-17 16:57:56 -0500

    Update login

--- src/scripts/components/auth/Facebook.js
+++ src/scripts/components/auth/Facebook.js
@@ -1,6 +1,5 @@
 "use strict";
 import React, { Component } from "react";
-import $ from "jquery";
 import { connect } from "react-redux";
 import { changeRoute } from "../../actions/routeActions";
 import { oauth } from "../../actions/authActions";
@@ -26,21 +25,6 @@ class Facebook extends Component {
         }, { scope: "public_profile,email,user_birthday" });
       };
     });
-  }
-
-  _handleLogin(response) {
-    // const url = "http://localhost:3000/api/auth/facebook/callback";
-    // $.ajax({
-    //   url: url,
-    //   xhrFields: { withCredentials: true },
-    //   async: false,
-    //   dataType: "json",
-    //   type: "GET",
-    //   success: (data, textStatus, xhr) => { console.log(data); },
-    //   error: (error, textStatus, errorThrown) => { console.error(error, textStatus, errorThrown); }
-    // });
-    // window.location = url;
-    // this.props.dispatch(changeRoute("MARKETPLACE"));
   }
 
   render() {
