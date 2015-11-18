9e25bb362f0d2ecc28d5b0590c996143dba8bc26
Dan Jensen <dj@fitbird.com>
2015-11-18 17:40:41 -0500

    update Auth0

--- src/scripts/components/auth/Auth0.js
+++ src/scripts/components/auth/Auth0.js
@@ -4,44 +4,17 @@ import React, { Component } from "react";
 export default class Auth0 extends Component {
   constructor(props) {
     super(props);
-    this.state = { idToken: {} };
-    this._showLock = this._showLock.bind(this);
+    this._handleClick = this._handleClick.bind(this);
   }
 
-  componentWillMount() {
-    this._createLock();
-    this.setState({ idToken: this._getIdToken()});
-  }
-
-  _createLock() {
-    this.lock = new Auth0Lock("yMLdR2C9Ojx2GlXs59oFHV9RLDezDmPJ", "fitbird.eu.auth0.com");
-  }
-
-  _getIdToken() {
-    let idToken = localStorage.getItem("userToken");
-    let authHash = this.lock.parseHash(window.location.hash);
-
-    if (!idToken && authHash) {
-      if (authHash.id_token) {
-        idToken = authHash.id_token;
-        localStorage.setItem("userToken", authHash.id_token);
-      }
-      if (authHash.error) {
-        console.error(authHash);
-        return null;
-      }
-    }
-    return idToken;
-  }
-
-  _showLock() {
-    this.lock.show();
+  _handleClick() {
+    this.props.lock.show();
   }
 
   render() {
     return (
       <div className="login-box">
-        <a onClick={this._showLock}>Login</a>
+        <a onClick={this._handleClick}>Login</a>
       </div>
     );
   }
---
+++ src/scripts/constants/auth0.js
@@ -0,0 +1,2 @@
+export const AUTH0_CLIENT_ID = "yMLdR2C9Ojx2GlXs59oFHV9RLDezDmPJ";
+export const AUTH0_DOMAIN = "fitbird.eu.auth0.com";
