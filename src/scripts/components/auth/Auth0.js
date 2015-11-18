928b08c03c6c9171a63081ee0dd66010b2bff791
Dan Jensen <dj@fitbird.com>
2015-11-18 14:37:04 -0500

    update Auth0

--- src/scripts/components/auth/Auth0.js
+++ src/scripts/components/auth/Auth0.js
@@ -4,17 +4,44 @@ import React, { Component } from "react";
 export default class Auth0 extends Component {
   constructor(props) {
     super(props);
-    this.showLock = this.showLock.bind(this);
+    this.state = { idToken: {} };
+    this._showLock = this._showLock.bind(this);
   }
 
-  showLock() {
-    this.props.lock.show();
+  componentWillMount() {
+    this._createLock();
+    this.setState({ idToken: this._getIdToken()});
+  }
+
+  _createLock() {
+    this.lock = new Auth0Lock("yMLdR2C9Ojx2GlXs59oFHV9RLDezDmPJ", "fitbird.eu.auth0.com");
+  }
+
+  _getIdToken() {
+    let idToken = localStorage.getItem("userToken");
+    let authHash = this.lock.parseHash(window.location.hash);
+
+    if (!idToken && authHash) {
+      if (authHash.id_token) {
+        idToken = authHash.id_token;
+        localStorage.setItem("userToken", authHash.id_token);
+      }
+      if (authHash.error) {
+        console.error(authHash);
+        return null;
+      }
+    }
+    return idToken;
+  }
+
+  _showLock() {
+    this.lock.show();
   }
 
   render() {
     return (
       <div className="login-box">
-        <a onClick={this.showLock}>Login</a>
+        <a onClick={this._showLock}>Login</a>
       </div>
     );
   }
