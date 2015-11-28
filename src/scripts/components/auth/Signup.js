81313673c5072e0b67e5bae660a0891eb8407c01
Dan Jensen <dj@fitbird.com>
2015-11-28 15:57:39 -0500

    remove google oauth

--- src/scripts/components/auth/Signup.js
+++ src/scripts/components/auth/Signup.js
@@ -7,9 +7,7 @@ import { connect } from "react-redux";
 import { changeRoute } from "../../actions/routeActions";
 import { signup } from "../../actions/authActions";
 import Auth0Signup from "./Auth0Signup";
-import Auth0Variables from "../../constants/auth0Variables";
 import Facebook from "./Facebook";
-import Google from "./Google";
 import IconButton from "../IconButton";
 import InputField from "../../components/InputField";
 import "./signup.css";
@@ -19,14 +17,6 @@ class Signup extends Component {
     super(props);
     this._handleClose = this._handleClose.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
-    this._signupCallback = this._signupCallback.bind(this);
-  }
-
-  componentWillMount() {
-    this.auth0 = new Auth0({
-      clientID: Auth0Variables.AUTH0_CLIENT_ID,
-      domain: Auth0Variables.AUTH0_DOMAIN
-    });
   }
 
   _handleClose() {
@@ -41,26 +31,12 @@ class Signup extends Component {
     let password = this.refs.password.state.fieldValue;
 
     if (email && name && password) {
-      this.auth0.signup({
-        sso: false,
-        auto_login: true,
-        connection: "Username-Password-Authentication",
+      this.props.dispatch(signup({
         email: email,
-        password: password
-      }, this._signupCallback);
+        password: password,
+        password_confirmation: passwordConfirmation
+      }));
     }
-  }
-
-  _signupCallback(error, profile, token) {
-    if (token) {
-      let data = {
-        user_metadata: { name: this.refs.name.state.fieldValue }
-      };
-      console.log("Signed up:", profile, token, data);
-    } else {
-      console.log("Error:", error);
-    }
-    // this.props.dispatch(signup(error, profile, token, data));
   }
 
   render() {
@@ -76,7 +52,6 @@ class Signup extends Component {
             <div className="mdl-card__supporting-text mdl-card--border">
               <div><Auth0Signup /></div>
               <div><Facebook /></div>
-              <div><Google /></div>
               <form onSubmit={this._handleSubmit}>
                 <div>
                   <InputField
