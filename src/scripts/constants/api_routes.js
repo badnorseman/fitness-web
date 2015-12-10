f7d3c0f8fb9a7e6aa73a8d77a626f382c56b1e4d
Dan Jensen <dj@fitbird.com>
2015-12-10 11:21:12 -0500

    update api routes

--- src/scripts/constants/api_routes.js
+++ src/scripts/constants/api_routes.js
@@ -1,7 +1,5 @@
 export const API = "https://api.fitbird.us/api";
-// export const API = "https://matrix-api.herokuapp.com/api"; // remote heroku
-// export const API = "http://localhost:5000/api"; // local puma
-// export const API = "http://localhost:3000/api"; // local rails server
+// export const API = "http://localhost:3000/api";
 export const LOGIN = `${API}/auth/identity/callback`;
 export const LOGOUT = `${API}/logout`;
 export const OAUTH = `${API}/auth`;
