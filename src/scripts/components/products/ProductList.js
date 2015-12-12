875d8b57391dd43684f7d63b4d76389903a5c6f9
Dan Jensen <dj@fitbird.com>
2015-12-12 10:44:22 -0500

    refactore ProductList to functional stateless component

--- src/scripts/components/products/ProductList.js
+++ src/scripts/components/products/ProductList.js
@@ -1,71 +1,53 @@
 "use strict";
-import React, { Component, PropTypes } from "react";
-import { render } from "react-dom";
 import ProductListItem from "./ProductListItem";
 
-export default class ProductList extends Component {
-  static propTypes = {
-    products: PropTypes.object,
-    onEdit: PropTypes.func.isRequired,
-    onNew: PropTypes.func.isRequired
-  }
-
-  constructor(props) {
-    super(props);
-    this._handleEdit = this._handleEdit.bind(this);
-    this._handleNew = this._handleNew.bind(this);
-  }
-
-  _getItems() {
+const ProductList = ({
+  products,
+  onEdit,
+  onNew
+}) => {
+  let items = (products) => {
     let items = [];
-    for (let key in this.props.products) {
-      if (this.props.products.hasOwnProperty(key)) {
+    for (let key in products) {
+      if (products.hasOwnProperty(key)) {
         items.push(
-          <ProductListItem key={key} item={this.props.products[key]} onSelect={this._handleEdit} />
+          <ProductListItem key={key} product={products[key]} onEdit={onEdit} />
         );
       }
     }
     return items;
-  }
+  }(products);
 
-  _handleEdit(product) {
-    this.props.onEdit(product);
-  }
+  const buttonStyle = {
+    float: "right",
+    margin: "50px 0 0 0"
+  };
+  const headerStyle = {
+    margin: "0 10px 10px 0",
+    maxWidth: "800px",
+    width: "25%"
+  };
+  const listStyle = {
+    padding: "50px 0 0 0"
+  };
 
-  _handleNew() {
-    this.props.onNew();
-  }
-
-  render() {
-    let items = this._getItems();
-    const buttonStyle = {
-      float: "right",
-      margin: "50px 0 0 0"
-    };
-    const headerStyle = {
-      margin: "0 10px 10px 0",
-      maxWidth: "800px",
-      width: "25%"
-    };
-    const listStyle = {
-      padding: "50px 0 0 0"
-    };
-    return (
-      <div className="block--center-horizontally__margin" style={listStyle}>
-        <div className="block--center-horizontally__flex">
-          <div style={headerStyle}>NAME</div>
-          <div style={headerStyle}>CURRENCY</div>
-          <div style={headerStyle}>PRICE</div>
-          <div style={headerStyle}>NUMBERS OF USERS</div>
-        </div>
-        {items}
-        <button
-          className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"
-          style={buttonStyle}
-          onClick={this._handleNew}>
-          <i className="material-icons">add</i>
-        </button>
+  return (
+    <div className="block--center-horizontally__margin" style={listStyle}>
+      <div className="block--center-horizontally__flex">
+        <div style={headerStyle}>NAME</div>
+        <div style={headerStyle}>CURRENCY</div>
+        <div style={headerStyle}>PRICE</div>
+        <div style={headerStyle}>NUMBERS OF USERS</div>
       </div>
-    )
-  }
-}
+      {items}
+      <button
+        className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"
+        style={buttonStyle}
+        onClick={onNew}>
+        <i className="material-icons">add</i>
+      </button>
+    </div>
+  );
+};
+
+export default ProductList;
