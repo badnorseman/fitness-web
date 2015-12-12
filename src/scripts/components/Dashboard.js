eb459a295212d3dafa23e4bd4cf58995a1206556
Dan Jensen <dj@fitbird.com>
2015-12-12 17:10:46 -0500

    refactore Dashboard to functional stateless component

--- src/scripts/components/Dashboard.js
+++ src/scripts/components/Dashboard.js
@@ -1,53 +1,39 @@
 "use strict";
-import React, { Component, PropTypes } from "react";
-import { render } from "react-dom";
+import React from "react";
 import ProductList from "./products/ProductList";
 import UserList from "./users/UserList";
 import "./dashboard.css";
 
-export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this._goToEditProduct = this._goToEditProduct.bind(this);
-    this._goToNewProduct = this._goToNewProduct.bind(this);
-  }
-
-  _goToEditProduct(product) {
-    this.props.changeRoute("EDITPRODUCT", product);
-  }
-
-  _goToNewProduct() {
-    this.props.changeRoute("NEWPRODUCT");
-  }
-
-  render() {
-    const { products, users } = this.props;
-
-    return (
-      <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--12-col">
-          <div className="dashboard-card block--center-horizontally__margin"><h3>Dashboard</h3></div>
-          <div className="dashboard-card block--center-horizontally__margin mdl-card mdl-shadow--2dp">
-            <div className="block--center-horizontally__margin mdl-card__supporting-text">
-              <div className="mdl-tabs mdl-js-tabs">
-                <div className="mdl-tabs__tab-bar">
-                  <a href="#products-panel" className="mdl-tabs__tab is-active">Products</a>
-                  <a href="#clients-panel" className="mdl-tabs__tab">Clients</a>
-                </div>
-                <div className="mdl-tabs__panel is-active" id="products-panel">
-                  <ProductList
-                    products={products}
-                    onEdit={this._goToEditProduct}
-                    onNew={this._goToNewProduct} />
-                </div>
-                <div className="mdl-tabs__panel" id="clients-panel">
-                  <UserList users={users} />
-                </div>
-              </div>
+const Dashboard = ({
+  products,
+  users,
+  onEdit,
+  onNew
+}) => (
+  <div className="mdl-grid">
+    <div className="mdl-cell mdl-cell--12-col">
+      <div className="dashboard-card block--center-horizontally__margin"><h3>Dashboard</h3></div>
+      <div className="dashboard-card block--center-horizontally__margin mdl-card mdl-shadow--2dp">
+        <div className="block--center-horizontally__margin mdl-card__supporting-text">
+          <div className="mdl-tabs mdl-js-tabs">
+            <div className="mdl-tabs__tab-bar">
+              <a href="#products-panel" className="mdl-tabs__tab is-active">Products</a>
+              <a href="#clients-panel" className="mdl-tabs__tab">Clients</a>
+            </div>
+            <div className="mdl-tabs__panel is-active" id="products-panel">
+              <ProductList
+                products={products}
+                onEdit={onEdit}
+                onNew={onNew} />
+            </div>
+            <div className="mdl-tabs__panel" id="clients-panel">
+              <UserList users={users} />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
+export default Dashboard;
