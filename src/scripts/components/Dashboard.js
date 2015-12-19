baa4319ba3c80de73f98e2483efe69bfebd6d3df
Dan Jensen <dj@fitbird.com>
2015-12-19 10:19:01 -0500

    merge dashboard component and container

--- src/scripts/components/Dashboard.js
+++ src/scripts/components/Dashboard.js
@@ -1,6 +1,4 @@
 "use strict";
-import React, { Component, PropTypes } from "react";
-import { render } from "react-dom";
 import { connect } from "react-redux";
 import { changeRoute } from "../actions/router_actions";
 import ProductList from "./products/ProductList";
@@ -25,41 +23,41 @@ const mapStateToProps = (state) => {
   };
 };
 
-class Dashboard extends Component {
-  render() {
-    const { products, users, onEdit, onNew } = this.props;
-
-    return (
-      <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--12-col">
-          <div className="dashboard-card block--center-horizontally__margin"><h3>Dashboard</h3></div>
-          <div className="dashboard-card block--center-horizontally__margin mdl-card mdl-shadow--2dp">
-            <div className="block--center-horizontally__margin mdl-card__supporting-text">
-              <div className="mdl-tabs mdl-js-tabs">
-                <div className="mdl-tabs__tab-bar">
-                  <a href="#products-panel" className="mdl-tabs__tab is-active">Products</a>
-                  <a href="#clients-panel" className="mdl-tabs__tab">Clients</a>
-                </div>
-                <div className="mdl-tabs__panel is-active" id="products-panel">
-                  <ProductList
-                    products={products}
-                    onEdit={onEdit}
-                    onNew={onNew}
-                  />
-                </div>
-                <div className="mdl-tabs__panel" id="clients-panel">
-                  <UserList
-                    users={users}
-                  />
-                </div>
-              </div>
+const Dashboard = ({
+  products,
+  users,
+  onEdit,
+  onNew
+}) => (
+  <div className="mdl-grid">
+    <div className="mdl-cell mdl-cell--12-col">
+      <div className="dashboard-card block--center-horizontally__margin"><h3>Dashboard</h3></div>
+      <div className="dashboard-card block--center-horizontally__margin mdl-card mdl-shadow--2dp">
+        <div className="block--center-horizontally__margin mdl-card__supporting-text">
+          <div className="mdl-tabs mdl-js-tabs">
+            <div className="mdl-tabs__tab-bar">
+              <a href="#products-panel" className="mdl-tabs__tab is-active">Products</a>
+              <a href="#clients-panel" className="mdl-tabs__tab">Clients</a>
+            </div>
+            <div className="mdl-tabs__panel is-active" id="products-panel">
+              <ProductList
+                products={products}
+                onEdit={onEdit}
+                onNew={onNew}
+              />
+            </div>
+            <div className="mdl-tabs__panel" id="clients-panel">
+              <UserList
+                users={users}
+              />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
 
 export default connect(
   mapStateToProps,
