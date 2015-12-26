cd31a45b6712e106afad57d71f05e7a1245d7b4a
Dan Jensen <dj@fitbird.com>
2015-12-26 10:36:47 -0500

    refactore Navigation

--- src/scripts/components/Navigation.js
+++ src/scripts/components/Navigation.js
@@ -6,107 +6,106 @@ import { changeRoute } from "../actions/router_actions";
 import { logout } from "../actions/auth_actions";
 import Link from "./Link";
 
-class Navigation extends Component {
-  _goToAccount = () => {
-    this.props.changeRoute("ACCOUNT");
-  };
+const Navigation = ({
+  currentUser,
+  goToAccount,
+  goToDashboard,
+  goToLogin,
+  goToSignup,
+  logout
+}) => {
+  const { avatar, coach, email, id, name } = currentUser;
+  const isLoggedIn = (id) ? true : false;
 
-  _goToDashboard = () => {
-    this.props.changeRoute("DASHBOARD");
-  };
+  return (
+    <nav className="mdl-navigation">
+      {!isLoggedIn && <div className="mdl-layout--large-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToLogin}>
+          Login
+        </Link>
+      </div>}
+      {!isLoggedIn && <div className="mdl-layout--large-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToSignup}>
+          Sign up
+        </Link>
+      </div>}
+      {coach && <div className="mdl-layout--large-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToDashboard}>
+          Dashboard
+        </Link>
+      </div>}
+      {isLoggedIn && <div className="mdl-layout--large-screen-only">
+        <div id="account-menu">
+          <img className="layout__header-avatar" src={avatar} alt="" />
+        </div>
+        <ul
+          className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+          htmlFor="account-menu">
+          <li>
+            <Link styles="mdl-menu__item" onClick={goToAccount}>
+              Account
+            </Link>
+          </li>
+          <li className="mdl-menu__item" onClick={logout}>Log out</li>
+        </ul>
+      </div>}
+      {!isLoggedIn && <div className="mdl-layout--small-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToLogin}>
+          <i className="material-icons">lock_open</i>
+        </Link>
+      </div>}
+      {!isLoggedIn && <div className="mdl-layout--small-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToSignup}>
+          <i className="material-icons">mood</i>
+        </Link>
+      </div>}
+      {coach && <div className="mdl-layout--small-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToDashboard}>
+          <i className="material-icons">dashboard</i>
+        </Link>
+      </div>}
+      {isLoggedIn && <div className="mdl-layout--small-screen-only">
+        <Link styles="mdl-navigation__link" onClick={goToAccount}>
+          <i className="material-icons">account_circle</i>
+        </Link>
+      </div>}
+      {isLoggedIn && <div className="mdl-layout--small-screen-only">
+        <Link styles="mdl-navigation__link" onClick={logout}>
+          <i className="material-icons">lock</i>
+        </Link>
+      </div>}
+    </nav>
+  );
+};
 
-  _goToLogin = () => {
-    this.props.changeRoute("LOGIN");
-  };
-
-  _goToSignup = () => {
-    this.props.changeRoute("SIGNUP");
-  };
-
-  _handleLogout = () => {
-    FB.getLoginStatus(response => {
-      if (response.status === "connected") {
-        FB.logout();
-      };
-    });
-    this.props.logout();
-  };
-
-  render() {
-    const { avatar, coach, email, id, name } = this.props.currentUser;
-    const isLoggedIn = (id) ? true : false;
-
-    return (
-      <div>
-        <nav className="mdl-navigation">
-          {!isLoggedIn && <div className="mdl-layout--large-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToLogin}>
-              Login
-            </Link>
-          </div>}
-          {!isLoggedIn && <div className="mdl-layout--large-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToSignup}>
-              Sign up
-            </Link>
-          </div>}
-          {coach && <div className="mdl-layout--large-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToDashboard}>
-              Dashboard
-            </Link>
-          </div>}
-          {isLoggedIn && <div className="mdl-layout--large-screen-only">
-            <div id="account-menu">
-              <img className="layout__header-avatar" src={avatar} alt="" />
-            </div>
-            <ul
-              className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-              htmlFor="account-menu">
-              <li>
-                <Link styles="mdl-menu__item" onClick={this._goToAccount}>
-                  Account
-                </Link>
-              </li>
-              <li className="mdl-menu__item" onClick={this._handleLogout}>Log out</li>
-            </ul>
-          </div>}
-          {!isLoggedIn && <div className="mdl-layout--small-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToLogin}>
-              <i className="material-icons">lock_open</i>
-            </Link>
-          </div>}
-          {!isLoggedIn && <div className="mdl-layout--small-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToSignup}>
-              <i className="material-icons">mood</i>
-            </Link>
-          </div>}
-          {coach && <div className="mdl-layout--small-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToDashboard}>
-              <i className="material-icons">dashboard</i>
-            </Link>
-          </div>}
-          {isLoggedIn && <div className="mdl-layout--small-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._goToAccount}>
-              <i className="material-icons">account_circle</i>
-            </Link>
-          </div>}
-          {isLoggedIn && <div className="mdl-layout--small-screen-only">
-            <Link styles="mdl-navigation__link" onClick={this._handleLogout}>
-              <i className="material-icons">lock</i>
-            </Link>
-          </div>}
-        </nav>
-      </div>
-    )
-  }
-}
+const logoutFacebook = () => {
+  FB.getLoginStatus(response => {
+    if (response.status === "connected") {
+      FB.logout();
+    };
+  });
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    goToAccount: () => {
+      dispatch(changeRoute("ACCOUNT"));
+    },
+    goToDashboard: () => {
+      dispatch(changeRoute("DASHBOARD"));
+    },
+    goToLogin: () => {
+      dispatch(changeRoute("LOGIN"));
+    },
+    goToSignup: () => {
+      dispatch(changeRoute("SIGNUP"));
+    },
     changeRoute: (route) => {
       dispatch(changeRoute(route));
     },
     logout: () => {
-      dispatch(logout);
+      logoutFacebook();
+      dispatch(logout());
     }
   };
 };
