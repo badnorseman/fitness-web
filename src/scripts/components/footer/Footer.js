b61ee94f968e1f76ce318621db86a1e3d79c410f
Dan Jensen <dj@fitbird.com>
2015-12-26 18:25:47 -0500

    refactore Footer

--- src/scripts/components/footer/Footer.js
+++ src/scripts/components/footer/Footer.js
@@ -1,29 +1,37 @@
 "use strict";
 import { connect } from "react-redux";
 import { changeRoute } from "../../actions/router_actions";
-import Copyright from "./Copyright";
 import Link from "../Link";
 
 const Footer = ({
   goToAbout,
   goToHelp,
   goToTerms
-}) => (
-  <footer className="mdl-mini-footer">
-    <div className="mdl-mini-footer--left-section">
-      <ul className="mdl-mini-footer--link-list">
-        <li><Link onClick={goToAbout}>About</Link></li>
-        <li><Link onClick={goToHelp}>Help</Link></li>
-        <li><Link onClick={goToTerms}>Privacy & Terms</Link></li>
-      </ul>
-    </div>
-    <div className="mdl-mini-footer--right-section">
-      <ul className="mdl-mini-footer--link-list">
-        <li><Copyright /></li>
-      </ul>
-    </div>
-  </footer>
-);
+}) => {
+  const now = new Date();
+  const currentYear = now.getFullYear();
+
+  return (
+    <footer className="mdl-mini-footer">
+      <div className="mdl-mini-footer--left-section">
+        <ul className="mdl-mini-footer--link-list">
+          <li><Link onClick={goToAbout}>About</Link></li>
+          <li><Link onClick={goToHelp}>Help</Link></li>
+          <li><Link onClick={goToTerms}>Privacy & Terms</Link></li>
+        </ul>
+      </div>
+      <div className="mdl-mini-footer--right-section">
+        <p>
+          Copyright&nbsp;&copy;&nbsp;2014-{currentYear}<br />
+          FitBird&nbsp;ApS<br />
+          Esromgade&nbsp;15&nbsp;&nbsp;Floor&nbsp;1&nbsp;&nbsp;Suite&nbsp;1102<br />
+          2200&nbsp;Copenhagen&nbsp;N&nbsp;&nbsp;Denmark<br />
+          CVR&nbsp;35418067
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
