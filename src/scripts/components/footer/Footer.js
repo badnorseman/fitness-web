0b061c88514c682e75d9b4f52131933bce8a3ec9
Dan Jensen <dj@fitbird.com>
2015-12-26 21:10:41 -0500

    refactore Footer

--- src/scripts/components/footer/Footer.js
+++ src/scripts/components/footer/Footer.js
@@ -12,24 +12,28 @@ const Footer = ({
   const currentYear = now.getFullYear();
 
   return (
-    <footer className="mdl-mini-footer">
-      <div className="mdl-mini-footer--left-section">
-        <ul className="mdl-mini-footer--link-list">
-          <li><Link onClick={goToAbout}>About</Link></li>
-          <li><Link onClick={goToHelp}>Help</Link></li>
-          <li><Link onClick={goToTerms}>Privacy & Terms</Link></li>
-        </ul>
+    <div>
+      <div className="mdl-mini-footer">
+        <div className="mdl-mini-footer--left-section">
+          <ul className="mdl-mini-footer--link-list">
+            <li><Link onClick={goToAbout}>About</Link></li>
+            <li><Link onClick={goToHelp}>Help</Link></li>
+            <li><Link onClick={goToTerms}>Privacy & Terms</Link></li>
+          </ul>
+        </div>
+        <div className="mdl-mini-footer--right-section">
+        </div>
       </div>
-      <div className="mdl-mini-footer--right-section">
-        <p>
-          Copyright&nbsp;&copy;&nbsp;2014-{currentYear}<br />
-          FitBird&nbsp;ApS<br />
-          Esromgade&nbsp;15&nbsp;&nbsp;Floor&nbsp;1&nbsp;&nbsp;Suite&nbsp;1102<br />
-          2200&nbsp;Copenhagen&nbsp;N&nbsp;&nbsp;Denmark<br />
+      <div className="mdl-mini-footer">
+        <span>
+          Copyright&nbsp;&copy;&nbsp;2014&ndash;{currentYear}&nbsp;&nbsp;
+          FitBird&nbsp;ApS&nbsp;&nbsp;
+          Esromgade&nbsp;15&nbsp;&nbsp;Suite&nbsp;1102&nbsp;&nbsp;
+          2200&nbsp;Copenhagen&nbsp;N&nbsp;&nbsp;Denmark&nbsp;&nbsp;
           CVR&nbsp;35418067
-        </p>
+        </span>
       </div>
-    </footer>
+    </div>
   );
 };
 
--- src/scripts/components/footer/Terms.js
+++ src/scripts/components/footer/Terms.js
@@ -4,7 +4,8 @@ const Terms = () => (
   <div className="mdl-grid">
     <div className="mdl-cell mdl-cell--12-col">
       <div className="mdl-card mdl-shadow--2dp">
-        <div className="mdl-card__title">
+        <div className="mdl-card__title block--center-horizontally__flex">
+          <div className="mdl-card__title-text">Terms</div>
         </div>
         <div className="mdl-card__supporting-text">
           Terms...
