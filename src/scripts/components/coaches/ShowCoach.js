04f7f41f993cb98905867049793ac2540cdc6a34
Dan Jensen <dj@fitbird.com>
2016-01-01 14:44:12 -0500

    update ShowCoach component

--- src/scripts/components/coaches/ShowCoach.js
+++ src/scripts/components/coaches/ShowCoach.js
@@ -16,16 +16,14 @@ const ShowCoach = ({
   const styles = {
     avatar: {
       borderRadius: "48px",
+      float: "right",
       height: "96px",
+      marginTop: "160px",
       width: "96px"
-    },
-    block: {
-      float: "left",
-      overflow: "hidden",
-      width: "50%"
     },
     card: {
       height: "auto",
+      minHeight: "320px",
       maxWidth: "800px",
       width: "800px"
     },
@@ -33,7 +31,9 @@ const ShowCoach = ({
       backgroundImage: "url(" + image + ")",
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
-      height: "160px",
+      height: "320px",
+      maxWidth: "800px",
+      width: "800px",
       WebkitTransition: "all",
       msTransition: "all"
     },
@@ -41,35 +41,26 @@ const ShowCoach = ({
 
   return (
     <div className="mdl-grid">
-      <div className="mdl-cell mdl-cell--12-col">
-        <div className="block--center-horizontally__margin mdl-card mdl-shadow--2dp"
-          style={styles.card}>
-          <div className="mdl-card__menu">
-            <Link
-              styles="mdl-button mdl-js-button mdl-button--icon"
-              onClick={onClose}
-            >
-              <i className="zmdi zmdi-close"></i>
-            </Link>
+      <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col block--center-horizontally__margin"
+        style={styles.image}>
+        <div className="mdl-card__supporting-text">
+          <img src={avatar} alt="" style={styles.avatar} />
+        </div>
+      </div>
+      <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col"
+        style={styles.card}>
+        <div className="mdl-card__supporting-text">
+          <div>
+            <h3>{name}</h3>
           </div>
-          <div className="mdl-card__title" style={styles.image}></div>
-          <div className="mdl-card__supporting-text">
-            <div className="block--center-horizontally__flex">
-              <div style={styles.block}>
-                <img src={avatar} alt="" style={styles.avatar} />
-              </div>
-              <div style={styles.block}>
-                <div>
-                  <h3>{name}</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mdl-card__supporting-text">
-            <div>
-              <h3>Contact</h3>
-              <p>{email}</p>
-            </div>
+        </div>
+      </div>
+      <div className="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col"
+        style={styles.card}>
+        <div className="mdl-card__supporting-text">
+          <div>
+            <h3>Contact</h3>
+            <p>{email}</p>
           </div>
         </div>
       </div>
