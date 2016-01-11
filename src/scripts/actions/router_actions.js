548cf8a15b20c9fec0505a352fadc555077c0388
Dan Jensen <dj@fitbird.com>
2016-01-11 14:44:44 -0500

    refactore Account to functional stateless component

--- src/scripts/actions/router_actions.js
+++ src/scripts/actions/router_actions.js
@@ -1,8 +1,20 @@
 "use strict";
 import * as  ACTION_TYPES from "../constants/action_types";
+import { getClients } from "./client_actions";
+import { getTransactions } from "./transaction_actions";
 
 const changeRoute = (route, param) => {
   return dispatch => {
+    switch (route) {
+      case "ACCOUNT":
+        dispatch(getTransactions());
+        break;
+      case "DASHBOARD":
+        console.log("get data to Dashboard");
+        break;
+      default:
+    }
+
     dispatch({
       type:  ACTION_TYPES.ROUTE_CHANGE,
       route: route,
