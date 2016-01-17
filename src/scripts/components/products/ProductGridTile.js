0ed417fee140ef25447971426f5fd909c2cb0a3d
Dan Jensen <dj@fitbird.com>
2016-01-17 12:45:25 -0500

    correct ProductGrid styles

--- src/scripts/components/products/ProductGridTile.js
+++ src/scripts/components/products/ProductGridTile.js
@@ -7,46 +7,32 @@ const ProductGridTile = ({
   const { currency, image, name, price } = product;
 
   const styles = {
-    cell: {
-      margin: "0"
-    },
-    card: {
-      height: "auto",
-      margin: "0 auto",
-      marginBottom: "10px",
-      marginRight: "10px",
-      maxWidth: "320px",
-      width: "320px"
-    },
-      image: {
-      backgroundImage: "url(" + image + ")",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      height: "160px",
-      WebkitTransition: "all",
-      msTransition: "all"
+    image: {
+      maxHeight: "160px"
     }
   };
 
   return (
-    <a href="#!"
-      onClick={ev => {
-        ev.preventDefault();
-        goTo("SHOWPRODUCT", product);
-      }}
-    >
-      <div className="mdl-cell mdl-cell--6-col-phone mdl-cell--4-col-tablet mdl-cell--3-col-desktop"
-        style={styles.cell}
-       >
-        <div className="mdl-card mdl-shadow--2dp" style={styles.card}>
-          <div className="mdl-card__title" style={styles.image}></div>
-          <div className="mdl-card__supporting-text">
-            <h5>{name}</h5>
-            <h6>{currency}&nbsp;{price}</h6>
-          </div>
-        </div>
+    <div className="mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--2dp">
+      <div className="mdl-card__media">
+        <a href="#!"
+          onClick={ev => {
+            ev.preventDefault();
+            goTo("SHOWPRODUCT", product);
+          }}
+        >
+          <img src={image} alt="" style={styles.image} />
+        </a>
       </div>
-    </a>
+      <div className="mdl-card__title">
+        <h5 className="mdl-card__title-text">{name}</h5>
+      </div>
+      <div className="mdl-card__supporting-text">
+        <h6 className="mdl-typography--subhead">
+          {currency}&nbsp;{price}
+        </h6>
+      </div>
+    </div>
   );
 };
 
