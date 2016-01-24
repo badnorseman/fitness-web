c3b97365814c69e7241ce41046ec22452c252536
Dan Jensen <dj@fitbird.com>
2016-01-24 13:23:25 -0500

    move color styling to layout.css

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -5,7 +5,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Layout from "./components/Layout";
-import "./App.css";
+import "./app.css";
 
 window.React = React;
 
