158c36d506af885374d8a750d21350ec2bc48aa1
Dan Jensen <dj@fitbird.com>
2016-02-04 12:14:12 -0500

    update Header with new Link

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,8 +1,8 @@
 "use strict";
-import Link from "./Link";
+import Link from "./Link2";
 import "./header.css";
 
-const Header = ({ currentUser, goTo, logout }) => {
+const Header = ({ currentUser, logout }) => {
   const logo = "https://s3.amazonaws.com/images.fitbird.us/development/system/57.png";
   const { avatar, coach, email, id, name } = currentUser;
   const isLoggedIn = (id) ? true : false;
@@ -11,24 +11,28 @@ const Header = ({ currentUser, goTo, logout }) => {
     <header className="mdl-layout__header mdl-layout__header--seamed">
       <div className="mdl-layout__header-row">
         <span className="mdl-layout-title">
-          <Link styles="mdl-navigation__link"
-            onClick={() => goTo("MARKETPLACE")}>
+          <Link route="MARKETPLACE" styles="mdl-navigation__link">
             <img className="header__logo" src={logo} alt="" />
             &nbsp;FITBIRD
           </Link>
         </span>
         <div className="mdl-layout-spacer"></div>
-        {coach && <Link styles="mdl-navigation__link"
-          onClick={() => goTo("DASHBOARD")}>Dashboard</Link>}
-        {coach && <Link styles="mdl-navigation__link"
-          onClick={() => goTo("PRODUCTLIST")}>Products</Link>}
-        {coach && <Link styles="mdl-navigation__link"
-          onClick={() => goTo("USERLIST")}>Customers</Link>}
+        {coach && <Link route="DASHBOARD" styles="mdl-navigation__link">
+          Dashboard
+        </Link>}
+        {coach && <Link route="PRODUCTLIST" styles="mdl-navigation__link">
+          Products
+        </Link>}
+        {coach && <Link route="USERLIST" styles="mdl-navigation__link">
+          Customers
+        </Link>}
         <nav className="mdl-navigation">
-          {!isLoggedIn && <Link styles="mdl-navigation__link"
-            onClick={() => goTo("LOGIN")}>Log in</Link>}
-          {!isLoggedIn && <Link styles="mdl-navigation__link"
-            onClick={() => goTo("SIGNUP")}>Sign up</Link>}
+          {!isLoggedIn && <Link route="LOGIN" styles="mdl-navigation__link">
+            Log in
+          </Link>}
+          {!isLoggedIn && <Link route="SIGNUP"styles="mdl-navigation__link">
+            Sign up
+          </Link>}
           {isLoggedIn && <div>
             <img className="header__avatar" src={avatar} alt="" />
             <button id="menu"
@@ -37,8 +41,9 @@ const Header = ({ currentUser, goTo, logout }) => {
             </button>
             <ul htmlFor="menu"
               className="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right">
-              <li><Link styles="mdl-menu__item"
-                onClick={() => goTo("ACCOUNT")}>Account</Link></li>
+              <li>
+                <Link route="ACCOUNT" styles="mdl-menu__item">Account</Link>
+              </li>
               <li className="mdl-menu__item" onClick={logout}>Log out</li>
             </ul>
           </div>}
