92a6f0390cf754257a76cba77cd586cf8100748f
Dan Jensen <dj@fitbird.com>
2016-02-08 20:07:41 -0500

    update EditProduct and NewProduct

--- src/scripts/components/products/EditProduct.js
+++ src/scripts/components/products/EditProduct.js
@@ -5,17 +5,11 @@ import Productform from "./Productform";
 
 const EditProduct = ({ product, dispatch }) => (
   <div className="mdl-grid">
-    <div className="mdl-cell mdl-cell--8-col-desktop mdl-cell--2-offset-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--2dp">
-      <div className="mdl-card__supporting-text">
-        <Productform
-          product={product}
-          onRemove={(id) => dispatch(destroyProduct(id))}
-          onSubmit={(product) => dispatch(updateProduct(product))}
-        />
-      </div>
-      <div className="mdl-card__actions mdl-card--border">
-      </div>
-    </div>
+    <Productform
+      product={product}
+      onRemove={(id) => dispatch(destroyProduct(id))}
+      onSubmit={(product) => dispatch(updateProduct(product))}
+    />
   </div>
 );
 
--- src/scripts/components/products/Productform.js
+++ src/scripts/components/products/Productform.js
@@ -41,63 +41,64 @@ class Productform extends Component {
     const styles = { image: { maxHeight: "160px" } };
 
     return (
-      <form>
-        <div>
-          <Inputfield
-            id="name" label="Name" type="text"
-            errorMessage="Letters and numbers and . , : - only. Start with a letter or number"
-            pattern="([a-zA-Z0-9]{1,}[.:-\s]{0,1})+?"
-            value={name}
-            ref="name" />
-        </div>
-        <div>
-          <Textarea
-            id="description" label="Description" type="text"
-            value={description}
-            ref="description" />
-        </div>
-        <div>
-          <Inputfield
-            id="price" label="Price" type="text"
-            errorMessage="Number and decimal mark only"
-            pattern="[0-9]{1,}((\.|\,)[0-9]{2,2})?"
-            value={price}
-            ref="price" />
-        </div>
-        <div>
-          <Selectfield
-            id="currency" label="Currency" options={currencies}
-            value={currency}
-            ref="currency" />
-        </div>
-        <img src={image} alt="" style={styles.image} />
-        <File ref="image" />
-        <div>
-          <button
-            className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-            onClick={this.handleSubmit}
-          >
-            Save
-          </button>
-          <div className="divider"></div>
-          <Link
-            route="PRODUCTLIST"
-            styles="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-          >
-            Cancel
-          </Link>
-          <div className="divider"></div>
-          {id && <button
-            className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
-            onClick={ev => {
-              ev.preventDefault();
-              this.props.onRemove(id);
-            }}
-          >
-            Delete
-          </button>}
-        </div>
-      </form>
+    <div className="mdl-cell mdl-cell--8-col-desktop mdl-cell--2-offset-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--2dp">
+      <div className="mdl-card__supporting-text">
+        <form>
+          <div>
+            <Inputfield
+              id="name" label="Name" type="text"
+              errorMessage="Letters and numbers and . , : - only. Start with a letter or number"
+              pattern="([a-zA-Z0-9]{1,}[.:-\s]{0,1})+?"
+              value={name}
+              ref="name" />
+          </div>
+          <div>
+            <Textarea
+              id="description" label="Description" type="text"
+              value={description}
+              ref="description" />
+          </div>
+          <div>
+            <Inputfield
+              id="price" label="Price" type="text"
+              errorMessage="Number and decimal mark only"
+              pattern="[0-9]{1,}((\.|\,)[0-9]{2,2})?"
+              value={price}
+              ref="price" />
+          </div>
+          <div>
+            <Selectfield
+              id="currency" label="Currency" options={currencies}
+              value={currency}
+              ref="currency" />
+          </div>
+          <img src={image} alt="" style={styles.image} />
+          <File ref="image" />
+        </form>
+      </div>
+      <div className="mdl-card__actions mdl-card--border">
+        <button
+          className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
+          onClick={this.handleSubmit}
+        >
+          Save
+        </button>
+        <Link
+          route="PRODUCTLIST"
+          styles="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
+        >
+          Cancel
+        </Link>
+        {id && <button
+          className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary"
+          onClick={ev => {
+            ev.preventDefault();
+            this.props.onRemove(id);
+        }}>
+          Delete
+        </button>}
+      </div>
+    </div>
     )
   }
 }
