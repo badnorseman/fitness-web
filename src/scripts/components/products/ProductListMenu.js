b1827159274d2ecc435ae326edd79e263182426b
Dan Jensen <dj@fitbird.com>
2016-02-09 18:38:42 -0500

    add ProductListMenu

--- src/scripts/components/products/ProductListMenu.js
+++ src/scripts/components/products/ProductListMenu.js
@@ -3,32 +3,28 @@ import { connect } from "react-redux";
 import { destroyProduct } from "../../actions/product_actions";
 import Link from "../Link";
 
-const ProductListMenu = ({ product, dispatch }) => {
-  const { id } = product;
-
-  return (
-    <div>
-      <button id={`product-list-menu${id}`}
-        className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon header__menu-button">
-        <i className="zmdi zmdi-more-vert"></i>
-      </button>
-      <ul htmlFor={`product-list-menu${id}`}
-        className="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right">
-        <li>
-          <Link route="EDITPRODUCT" param={product} styles="mdl-menu__item">
-            Edit
-          </Link>
-        </li>
-        <li className="mdl-menu__item"
-          onClick={ev => {
-            ev.preventDefault();
-            dispatch(destroyProduct(id));
-        }}>
-          Delete
-        </li>
-      </ul>
-    </div>
-  );
-};
+const ProductListMenu = ({ product, dispatch }) => (
+  <div>
+    <button id={`product-list-menu${product.id}`}
+      className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon header__menu-button">
+      <i className="zmdi zmdi-more-vert"></i>
+    </button>
+    <ul htmlFor={`product-list-menu${product.id}`}
+      className="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right">
+      <li>
+        <Link route="EDITPRODUCT" param={product} styles="mdl-menu__item">
+          Edit
+        </Link>
+      </li>
+      <li className="mdl-menu__item"
+        onClick={ev => {
+          ev.preventDefault();
+          dispatch(destroyProduct(product.id));
+      }}>
+        Delete
+      </li>
+    </ul>
+  </div>
+);
 
 export default connect()(ProductListMenu)
